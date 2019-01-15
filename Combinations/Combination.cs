4f45a5a2cdb3382468e37eb55f0d3ffecaba0722
Adrian Tarniceru <Adrian.Tarniceru@Honeywell.com>
2019-01-15 13:19:23 +0200

    Combination class created

---
+++ Combinations/Combination.cs
@@ -0,0 +1,16 @@
+﻿namespace Combinations
+{
+    public class Combination
+    {
+        private int CombinedElements { get; set; }
+
+        private int[] elements;
+
+        public Combination(int combinedElements)
+        {
+            CombinedElements = combinedElements;
+
+            elements = new int[combinedElements + 1];
+        }
+    }
+}
